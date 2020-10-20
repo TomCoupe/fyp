20bf992f48e309c5b52624e12beb3ad79de568ed
Tom Coupe <thomas.coupe@yahoo.co.uk>
2020-10-20 14:37:38 +0100

    importing scripts and app.js from a seperate blade

---
+++ resources/views/home/homepage.blade.php
@@ -0,0 +1,9 @@
+@extends('welcome')
+@section('content')
+<div>
+    <div class="center-width">
+        <img src="images/gamelogo.png">
+    </div>
+</div>
+    <homepage-buttons></homepage-buttons>
+@endsection
