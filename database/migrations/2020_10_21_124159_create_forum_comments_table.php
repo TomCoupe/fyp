610f0de1c790e6a1ec79bc760f233c49057b78dc
Tom Coupe <thomas.coupe@yahoo.co.uk>
2020-10-21 13:47:23 +0100

    forum comments db stuff

--- database/migrations/2020_10_21_124159_create_forum_comments_table.php
+++ database/migrations/2020_10_21_124159_create_forum_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateForumPostsTable extends Migration
+class CreateForumCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateForumPostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('forum_posts', function (Blueprint $table) {
+        Schema::create('forum_comments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->integer('user_id');
+            $table->integer('forum_post_id');
             $table->string('text');
+            $table->integer('user_id');
             $table->integer('likes');
             $table->integer('dislikes');
         });
@@ -30,6 +31,6 @@ class CreateForumPostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('forum_posts');
+        Schema::dropIfExists('forum_comments');
     }
 }
