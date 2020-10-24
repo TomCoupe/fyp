2e16a152f2ad208b235a3d07a37b775bb101788c
Tom Coupe <thomas.coupe@yahoo.co.uk>
2020-10-24 15:38:47 +0100

    adjusting login / register pages to match dark theme

--- resources/js/components/GameCanvas.vue
+++ resources/js/components/GameCanvas.vue
@@ -1,5 +1,5 @@
 <template>
-    <div >
+    <div>
         <canvas class="center-div" id="game" width="640" height="640"></canvas>
     </div>
 </template>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('welcome')
 
 @section('content')
-<div class="container">
+<div class="container center-div">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -63,7 +63,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-dark">
                                     {{ __('Register') }}
                                 </button>
                             </div>
