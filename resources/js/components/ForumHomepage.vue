dbbf6f492e7d9a1d59f24dde35eff936d6d0a91b
Tom Coupe <thomas.coupe@yahoo.co.uk>
2020-10-28 13:38:15 +0000

    starting the forum homepage UI

--- resources/js/components/ForumHomepage.vue
+++ resources/js/components/ForumHomepage.vue
@@ -1,13 +1,22 @@
 <template>
     <div>
-        
+        <template v-for="post in forumPosts">
+            <div class="card">
+                <h5 class="card-header">Featured</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
+        </template>
     </div>
 </template>
 
 <script>
 export default {
     name: 'ForumHomepage.vue',
-    // props: ['user'],
+    props: ['user', 'posts'],
 
     mounted() {
         console.log("Component mounted.");
@@ -15,7 +24,8 @@ export default {
 
     data() {
         return {
-            // player: this.user,
+            player: this.user,
+            forumPosts: this.posts,
         };
     }
 };
