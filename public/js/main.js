fef3d78c2375a7e366feaaaded023cf5d2f8b6c0
Tom Coupe <thomas.coupe@yahoo.co.uk>
2020-11-05 12:52:07 +0000

    setting up js file for functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,11 @@
-// import { loadImage } from "./helpers.js";
+ import { loadImage } from "/js/helpers.js";
 
-
-function loadImage(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', () => {
-            resolve(image);
-        })
-        image.src = url;
-    }); 
-}
 
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
 
-// context.fillRect(0, 0, 640, 640);  
+context.fillRect(0, 0, 50, 50);  
 
 loadImage('/images/game/tiles.png').then(image => {
-    context.drawImage(image, 0 ,0, 16, 16, 0, 0, 16, 16);
+    context.drawImage(image, 0, 0);
 })
