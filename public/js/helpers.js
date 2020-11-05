207e927dc69c7aa8798fa3394dbdaba2d2ec782f
Tom Coupe <thomas.coupe@yahoo.co.uk>
2020-11-05 14:02:40 +0000

    small canvas. but got background sprites loading in

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -5,5 +5,30 @@ export function loadImage(url) {
             resolve(image);
         })
         image.src = url;
-    }); 
+    });
 }
+
+export function define(name, x, y, width, height, image, tiles) {
+    const buffer = document.createElement('canvas');
+    buffer.width = width;
+    buffer.height = height;
+    buffer.getContext('2d').drawImage(
+        image,
+        x * width,
+        y * height,
+        width,
+        height,
+        0,
+        0,
+        width,
+        height
+    );
+    tiles.set(name, buffer);
+}
+
+export function draw(name, context, x, y, tiles) {
+    const buffer = tiles.get(name);
+    context.drawImage(buffer, x, y);
+}
+
+
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
- import { loadImage } from "/js/helpers.js";
+ import { loadImage, draw, define } from "/js/helpers.js";
+//  import { define } from "/js/helpers.js";
 
 
 const canvas = document.getElementById('game');
@@ -7,5 +8,18 @@ const context = canvas.getContext('2d');
 context.fillRect(0, 0, 50, 50);  
 
 loadImage('/images/game/tiles.png').then(image => {
-    context.drawImage(image, 0, 0);
+    const tiles = new Map();
+    define('ground', 0, 0, 16, 16, image, tiles);
+    define('sky', 3, 23, 16, 16, image, tiles);
+    draw('ground', context, 45, 62, tiles);
+
+    for(let x = 0; x < 25; x++) {
+        for(let y = 0; y < 14; y++) {
+            draw('sky', context, x * 16, y * 16, tiles);
+
+            if(y >= 12) {
+                draw('ground', context, x * 16, y * 16, tiles);
+            }
+        }
+    }
 })
