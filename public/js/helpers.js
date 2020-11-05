74323182f7b828de3a3fd4f6bf090b2f9d842baf
Tom Coupe <thomas.coupe@yahoo.co.uk>
2020-11-05 15:19:45 +0000

    js cleanup

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -31,4 +31,23 @@ export function draw(name, context, x, y, tiles) {
     context.drawImage(buffer, x, y);
 }
 
+export function drawBackground(background, context, tiles, width, height) {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+        for(let x = x1; x < x2; x++) {
+            for(let y = y1; y < y2; y++) {
+                drawTile(background.tile, context, x, y, width, height, tiles);
+            }
+        }
+    })
+}
+
+export function drawTile(name, context, x, y, width, height, tiles) {
+    draw(name, context, x * width, y * height, tiles)
+}
+
+export function loadLevel(name) {
+    return fetch('/levels/' + name +'.json')
+    .then(r => r.json());
+}
+
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,19 @@
- import { loadImage, draw, define } from "/js/helpers.js";
-//  import { define } from "/js/helpers.js";
-
+import { loadImage, draw, define, loadLevel, drawTile, drawBackground } from "/js/helpers.js";
 
 const canvas = document.getElementById('game');
-const context = canvas.getContext('2d');
-
-context.fillRect(0, 0, 50, 50);  
+const context = canvas.getContext('2d'); 
+const tiles = new Map();
 
 loadImage('/images/game/tiles.png').then(image => {
-    const tiles = new Map();
     define('ground', 0, 0, 16, 16, image, tiles);
     define('sky', 3, 23, 16, 16, image, tiles);
     draw('ground', context, 45, 62, tiles);
 
-    for(let x = 0; x < 25; x++) {
-        for(let y = 0; y < 14; y++) {
-            draw('sky', context, x * 16, y * 16, tiles);
-
-            if(y >= 12) {
-                draw('ground', context, x * 16, y * 16, tiles);
-            }
-        }
-    }
+    loadLevel('level-1')
+    .then(level => {
+        level.backgrounds.forEach(background => {
+            drawBackground(background, context, tiles, 16, 16);
+        })
+        
+    })
 })
