75e395efd11ce8b79a2757d3dfbb98e7f1717116
Tom Coupe <thomas.coupe@yahoo.co.uk>
2021-02-14 17:17:41 +0000

    player now loading from json.

--- public/js/entities.js
+++ public/js/entities.js
@@ -2,18 +2,17 @@ import Entity from './Entity.js';
 import Jump from './traits/Jump.js';
 import Move from './traits/Move.js';
 import Velocity from './traits/Velocity.js';
-import {loadPlayerSprite} from './sprites.js';
+import {loadSpriteSheet} from './loaders.js';
 
 export function createCharacter() {
-    return loadPlayerSprite()
+    return loadSpriteSheet('player')
     .then(sprite => {
-    
+        console.log(sprite);
         const player = new Entity();
 
         player.size.set(14, 16);
         player.addTrait(new Move());
         player.addTrait(new Jump());
-        // mario.addTrait(new Velocity());
 
         player.draw = function drawMario(context) {
             sprite.draw('idle', context, 0, 0);
--- public/js/loaders.js
+++ public/js/loaders.js
@@ -16,22 +16,31 @@ function loadJSON(path) {
     return fetch(path).then(r => r.json())
 }
 
-function loadSpriteSheet(name) {
+export function loadSpriteSheet(name) {
 
-    return loadJSON('/sprites/'+ name +'.json').then(spriteSheet => Promise.all([
+    return loadJSON('/sprites/' + name + '.json').then(spriteSheet => Promise.all([
         spriteSheet,
         loadImage(spriteSheet.imagePath)
     ])).then(([spriteSheet, image]) => {
         console.log(spriteSheet)
         const sprites = new SpriteSheet(image, spriteSheet.tileWidth, spriteSheet.tileHeight)
-        spriteSheet.tiles.forEach(tile => {
-            sprites.defineTile(tile.name, tile.index[0], tile.index[1])
-        })
+
+        if (spriteSheet.tiles) {
+            spriteSheet.tiles.forEach(tile => {
+                sprites.defineTile(tile.name, tile.index[0], tile.index[1])
+            })
+        }
+
+        if (spriteSheet.frames) {
+            spriteSheet.frames.forEach(frame => {
+                sprites.define(frame.name, frame.rect[0], frame.rect[1], frame.rect[2], frame.rect[3]);
+            })
+        }
         return sprites;
     })
-}   
+}
 
-export function createTiles(level, backgrounds){
+export function createTiles(level, backgrounds) {
 
     function applyRange(background, xPos, xLen, yPos, yLen) {
         for (let x = xPos; x < xPos + xLen; ++x) {
@@ -43,30 +52,30 @@ export function createTiles(level, backgrounds){
             }
         }
     }
-        //looping through each individual tile and adding it to the tile matrix
-        backgrounds.forEach(background => {
-            background.ranges.forEach(range => {
-                if (range.length === 4) {
-                    const [xPos, xLen, yPos, yLen] = range;
-                    applyRange(background, xPos, xLen, yPos, yLen)
-                }
-                if(range.length === 3) {
-                    const [xPos, xLen, yPos] = range;
-                    applyRange(background, xPos, xLen, yPos, 1)
-                }
-                if (range.length === 2) {
-                    const [xPos, yPos] = range;
-                    applyRange(background, xPos, 1, yPos, 1)
-                }
-            });
-        })
-    }
+    //looping through each individual tile and adding it to the tile matrix
+    backgrounds.forEach(background => {
+        background.ranges.forEach(range => {
+            if (range.length === 4) {
+                const [xPos, xLen, yPos, yLen] = range;
+                applyRange(background, xPos, xLen, yPos, yLen)
+            }
+            if (range.length === 3) {
+                const [xPos, xLen, yPos] = range;
+                applyRange(background, xPos, xLen, yPos, 1)
+            }
+            if (range.length === 2) {
+                const [xPos, yPos] = range;
+                applyRange(background, xPos, 1, yPos, 1)
+            }
+        });
+    })
+}
 
 
 export function loadLevel(name) {
     return Promise.all([
         loadJSON(`/levels/${name}.json`),
-        loadSpriteSheet('1-1')  
+        loadSpriteSheet('1-1')
     ]).then(([levelSpec, backgroundSprite]) => {
         console.log(backgroundSprite);
         const level = new Level();
