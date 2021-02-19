fde5e0ab3ae983a094e5369faf3a7f677e3ca192
Tom Coupe <thomas.coupe@yahoo.co.uk>
2021-02-19 14:04:56 +0000

    enemy now walking properly

--- public/js/traits/EnemyWalk.js
+++ public/js/traits/EnemyWalk.js
@@ -1,57 +1,49 @@
-import {Trait} from '../Entity.js';
+import { Trait } from '../Entity.js';
 
 export default class EnemyWalk extends Trait {
     constructor() {
         super('enemyWalk');
-    
+
         this.direction = 1;
         this.speed = 5000;
         this.distance = 0;
-        this.directionFacing = 1;
+        this.directionFacing = true;
         this.lastPos = 0;
     }
 
     getDirection() {
-        if(this.direction == 1) {
-            return -1;
+        if (this.direction == 1) {
+            return false;
         }
-        return 1;
+        return true;
     }
 
     checkDirection(entity) {
-        if(this.direction = 1) {
+        if (this.direction = 1) {
             if (entity.pos.x > this.lastPos) {
-                return true;
-            }
-            return false; 
-        }
-        if(this.direction -1) {
-            if(entity.pos.x <= this.lastPos) {
                 return true;
             }
             return false;
         }
-        
+        if (this.direction =  -1) {
+            if (entity.pos.x <= this.lastPos) {
+                return true;
+            }
+            return false;
+        }
+
     }
 
     update(entity, deltaTime) {
-        // console.log(this.direction)
-        // console.log(entity.pos.x, this.lastPos)
-
-        console.log(this.checkDirection(entity))
         entity.vel.x = this.speed * this.direction * deltaTime;
-        if (entity.pos.x > this.lastPos) {
-            console.log('test');
-            // console.log(entity.pos.x, this.lastPos)
-            // console.log(this.distance);
-            // this.directionFacing = this.direction;
+        console.log(entity.vel.x);
+        if (entity.pos.x == this.lastPos) {
+            this.direction = -this.direction;
+            this.lastPos = -1;
             this.distance += Math.abs(entity.vel.x * deltaTime);
-        }
+        } 
         else {
-            console.log('das')
-            this.direction = this.getDirection(); 
+            this.lastPos = entity.pos.x;
         }
-
-        this.lastPos = entity.pos.x;
     }
 }
