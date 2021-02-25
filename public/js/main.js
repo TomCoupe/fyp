77c5e1ee01b6c7d5cb288df9465c126ad8e3427b
Tom Coupe <thomas.coupe@yahoo.co.uk>
2021-02-25 15:54:20 +0000

    level system working much better. now loading level 2 when win block is reached

--- public/js/main.js
+++ public/js/main.js
@@ -15,27 +15,18 @@ const LEVEL1 = '1-1';
 const LEVEL2 = '1-2';
 const LEVEL3 = '1-3';
 
-// var levelState = 1;
+var currentLevel = 1;
 
 context.scale(2.5, 2.5);
 
 const levelHandler = new LevelHandler();
 
 
-function levelState() {
-    switch (levelHandler.getLevel()) {
-        case 1:
-            level1();
-            break;
-        case 2:
-            level2();
-            break;
-        case 3:
-            break
-    }
+if (currentLevel == 1) {
+    level1();
+} else if (currentLevel == 2) {
+    level2();
 }
-
-levelState();
 
 
 function level1() {
@@ -55,6 +46,8 @@ function level1() {
 
         character.playerReset();
 
+        character.pos.set(1350, 170)
+
         level.game.layers.push(createCollisionLayer(level), createScreenLayer(screen));
         level.entities.add(enemy1);
         level.entities.add(enemy2);
@@ -65,30 +58,30 @@ function level1() {
 
         const timer = new Timer(1 / 60);
         timer.update = function update(deltaTime) {
-            
-            level.update(deltaTime, screen, levelHandler);
+            if (currentLevel == 1) {
+                level.update(deltaTime, screen, levelHandler);
 
-            if (character.pos.x > 100) {
-                screen.position.x = character.pos.x - 100;
-            }
+                if (character.pos.x > 100) {
+                    screen.position.x = character.pos.x - 100;
+                }
 
-            level.game.draw(context, screen);
-            // console.log(Math.floor(enemy1.pos.x));
+                level.game.draw(context, screen);
 
-            if (Math.floor(enemy1.pos.x) === Math.floor(character.pos.x) && Math.floor(enemy1.pos.y) === Math.floor(character.pos.y)) {
-                character.pos.set(64, 64);
-                screen.position.set(0, 0);
-            }
+                if (Math.round(enemy1.pos.x) === Math.round(character.pos.x) && Math.round(enemy1.pos.y) === Math.round(character.pos.y)) {
+                    character.pos.set(64, 64);
+                    screen.position.set(0, 0);
+                }
 
-            character.vel.y += gravity * deltaTime;
-            console.log('beeef');
+                character.vel.y += gravity * deltaTime;
 
-            if(Math.floor(character.pos.x) == 1407 && Math.floor(character.pos.y) == 176) {
-                level2();
-                return;
+                if (Math.floor(character.pos.x) == 1407 && Math.floor(character.pos.y) == 176) {
+                    timer.stop();
+                    currentLevel = 2;
+                    level2();
+                    return;
+                }
             }
         }
-
         timer.start();
     });
 }
@@ -96,12 +89,18 @@ function level1() {
 function level2() {
     Promise.all([
         createCharacter(),
-        loadLevel(LEVEL1)
+        loadLevel(LEVEL2)
     ]).then(([character, level]) => {
         const gravity = 2000;
         const screen = new Screen();
 
         character.playerReset();
+
+        if (!Window.event) {
+            watchKeyBoard(character);
+        }
+
+        console.log(character.vel.x);
 
         level.game.layers.push(createCollisionLayer(level), createScreenLayer(screen));
 
@@ -109,22 +108,18 @@ function level2() {
 
         const timer = new Timer(1 / 60);
         timer.update = function update(deltaTime) {
-            let gameState = level.update(deltaTime, screen);
+            if (currentLevel == 2) {
+                level.update(deltaTime, screen);    
 
-            if (gameState == true) {
-                LEVEL_STATE = LEVEL_STATE++
+                if (character.pos.x > 100) {
+                    screen.position.x = character.pos.x - 100;
+                }
+
+                level.game.draw(context, screen);
+
+                character.vel.y += gravity * deltaTime;
             }
-
-            if (character.pos.x > 100) {
-                screen.position.x = character.pos.x - 100;
-            }
-
-            level.game.draw(context, screen);
-
-            character.vel.y += gravity * deltaTime;
         }
-
-        watchKeyBoard(character);
 
         timer.start();
     });
